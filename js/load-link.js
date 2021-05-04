686a15954f4ac434c4b68ae44777993253d06bb7
Nick061293 <nicolas.tomita@etec.sp.gov.br>
2021-05-03 21:29:29 -0300

    Att

---
+++ js/load-link.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.nav-link').click(function(e) {
+        e.preventDefault();
+        //pegar o link
+        let url = $(this).attr('href')
+            //limpar a div
+        $('#conteudo').empty()
+        $('#conteudo').load('url')
+    })
+})
