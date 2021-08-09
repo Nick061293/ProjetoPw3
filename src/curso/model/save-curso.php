9057a48c2a62a844bb620848bb42bea0fcb10fdf
Nick061293 <nicolas.tomita@etec.sp.gov.br>
2021-08-09 14:38:51 -0300

    Curso att

--- src/curso/model/save-curso.php
+++ src/curso/model/save-curso.php
@@ -7,7 +7,7 @@
     $requestData = $_REQUEST;
 
     // Verificação de campo obrigatórios do formulário
-    if(empty($requestData['DESCRICAO'])){
+    if(empty($requestData['NOME'])){
         // Caso a variável venha vazia eu gero um retorno de erro do mesmo
         $dados = array(
             "tipo" => 'error',
@@ -15,43 +15,45 @@
         );
     } else {
         // Caso não exista campo em vazio, vamos gerar a requisição
-        $IDTIPO_USUARIO = isset($requestData['IDTIPO_USUARIO']) ? $requestData['IDTIPO_USUARIO'] : '';
+        $IDCURSO = isset($requestData['IDCURSO']) ? $requestData['IDCURSO'] : '';
         $operacao = isset($requestData['operacao']) ? $requestData['operacao'] : '';
 
         // Verifica se é para cadastra um nvo registro
         if($operacao == 'insert'){
             // Prepara o comando INSERT para ser executado
             try{
-                $stmt = $pdo->prepare('INSERT INTO TIPO_USUARIO (DESCRICAO) VALUES (:nomeTipo)');
+                $stmt = $pdo->prepare('INSERT INTO CURSO (NOME, EIXO_IDEIXO) VALUES (:a, :b)');
                 $stmt->execute(array(
-                    ':nomeTipo' => utf8_decode($requestData['DESCRICAO'])
+                    ':a' => utf8_decode($requestData['NOME']),
+                    ':b' => $requestData['EIXO_IDEIXO']
                 ));
                 $dados = array(
                     "tipo" => 'success',
-                    "mensagem" => 'Tipo de usuário cadastrado com sucesso.'
+                    "mensagem" => 'Curso foi cadastrado com sucesso.'
                 );
             } catch(PDOException $e) {
                 $dados = array(
                     "tipo" => 'error',
-                    "mensagem" => 'Não foi possível efetuar o cadastro do tipo do usuário.'
+                    "mensagem" => 'Não foi possível efetuar o cadastro do curso.'
                 );
             }
         } else {
             // Se minha variável operação estiver vazia então devo gerar os scripts de update
             try{
-                $stmt = $pdo->prepare('UPDATE TIPO_USUARIO SET DESCRICAO = :nomeTipo WHERE IDTIPO_USUARIO = :id');
+                $stmt = $pdo->prepare('UPDATE CURSO SET NOME = :a, EIXO_IDEIXO = :b WHERE IDCURSO = :id');
                 $stmt->execute(array(
-                    ':id' => $IDTIPO_USUARIO,
-                    ':nomeTipo' => utf8_decode($requestData['DESCRICAO'])
+                    ':id' => $IDCURSO,
+                    ':a' => utf8_decode($requestData['NOME']),
+                    ':b' => $requestData['EIXO_IDEIXO']
                 ));
                 $dados = array(
                     "tipo" => 'success',
-                    "mensagem" => 'Tipo de usuário atualizado com sucesso.'
+                    "mensagem" => 'O curso foi atualizado com sucesso.'
                 );
             } catch (PDOException $e) {
                 $dados = array(
                     "tipo" => 'error',
-                    "mensagem" => 'Não foi possível efetuar a alteração do tipo do usuário.'
+                    "mensagem" => 'Não foi possível efetuar a alteração do curso.'
                 );
             }
         }
